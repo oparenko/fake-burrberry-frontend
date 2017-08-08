c856aa16107c6e2c0a32497d167f7f048671902c
oparenko <yuriy@oparenko.com>
2017-08-08 21:43:03 +0100

    feat: connected webfonts

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import 'flexboxgrid2';
-
+import WebFont from 'webfontloader';
 import './index.css';
 import Header from './Header';
 import registerServiceWorker from './registerServiceWorker';
+
+WebFont.load({
+  google: {
+    families: ['Lora:400,400i', 'serif', 'Raleway:400,500,600,700&amp;subset=cyrillic', 'sans-serif']
+  }
+});
 
 ReactDOM.render(<Header />, document.getElementById('root'));
 registerServiceWorker();

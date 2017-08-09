149e424412c0bdcd2fc0f3c86ae2383c2b418ec5
oparenko <yuriy@oparenko.com>
2017-08-09 22:55:00 +0100

    feat: add helmet for handling titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Helmet } from 'react-helmet';
+
 import Header from "./Header.jsx";
 import Main from "./Main";
 import Footer from "./Footer.jsx";
@@ -7,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Helmet defaultTitle="Burrberry" titleTemplate="%s Burrberry"/>
         <Header />
         <Main />
         <Footer />

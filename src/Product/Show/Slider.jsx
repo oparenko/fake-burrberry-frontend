79d579a7360c3ac341cc5cd586f719d9dd95bde2
oparenko <yuriy@oparenko.com>
2017-08-31 20:24:54 +0100

    fix: use images from burberry website

--- src/Product/Show/Slider.jsx
+++ src/Product/Show/Slider.jsx
@@ -37,19 +37,19 @@ const Photo = styled.img`
 export default () =>
   (<Slider>
     <Photo
-      src={`${process.env.PUBLIC_URL}/img/Long-Cotton-Gabardine-Car-Coat-1.jpg`}
+      src="https://assets.burberry.com/is/image/Burberryltd/98b658a12f42e12facf56ca59c87a33084b76231.jpg?$BBY_V2_ML_3X4$&hei=960&wid=720"
       alt="Long Cotton Gabardine Car Coat"
     />
     <Photo
-      src={`${process.env.PUBLIC_URL}/img/Long-Cotton-Gabardine-Car-Coat-2.jpg`}
+      src="https://assets.burberry.com/is/image/Burberryltd/ac883de18f98ce6c630436cc95b594998dcfb5d6.jpg?$BBY_V2_ML_3X4$&hei=960&wid=720"
       alt="Long Cotton Gabardine Car Coat"
     />
     <Photo
-      src={`${process.env.PUBLIC_URL}/img/Long-Cotton-Gabardine-Car-Coat-3.jpg`}
+      src="https://assets.burberry.com/is/image/Burberryltd/7c5bb44b566ce3a820cfdc8cacb377065d540e95.jpg?$BBY_V2_SL_3X4$&hei=960&wid=720"
       alt="Long Cotton Gabardine Car Coat"
     />
     <Photo
-      src={`${process.env.PUBLIC_URL}/img/Long-Cotton-Gabardine-Car-Coat-4.jpg`}
+      src="https://assets.burberry.com/is/image/Burberryltd/1e8677ea552b01b2e33caa153088a5c59d25a462.jpg?$BBY_V2_ML_3X4$&hei=960&wid=720"
       alt="Long Cotton Gabardine Car Coat"
     />
   </Slider>);
